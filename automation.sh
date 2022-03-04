2529b96a647a93f0d90b447946b67dd85994ce8b
yashwanthkota <43819527+yashwanthkota@users.noreply.github.com>
2022-03-05 01:13:07 +0530

    Updated automation.sh with the cron job as well as with the inventory.html
    Updated automation.sh with the cron job as well as with the inventory.html

--- automation.sh
+++ automation.sh
@@ -19,3 +19,13 @@ cp $myname-httpd-logs-$timestamp.tar /tmp/
 aws s3 \
 cp /tmp/$myname-httpd-logs-$timestamp.tar \
 s3://$s3_bucket/$myname-httpd-logs-$timestamp.tar
+file="/tmp/$myname-httpd-logs-$timestamp.tar"
+test -f /var/www/html/inventory.html || echo -e "Log Type\tTime Created\tType\tSize" > /var/www/html/inventory.html
+echo -e  "httpd-logs\t$timestamp\ttar\t$(sudo du $file | awk '{print $1,"K"}')" >> /var/www/html/inventory.html
+
+if  grep "automation.sh" /etc/cron.d/automation
+then
+        echo "Cron job exists"
+else
+        echo "15 15 * * * root /home/ubuntu/Automation_Project/automation.sh" > /etc/cron.d/automation
+fi
